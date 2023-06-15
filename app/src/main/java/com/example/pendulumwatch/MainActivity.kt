62c504cc832834e95877c313f935c876c754d84a
CherifiMi <cherifi154949@gmail.com>
2023-06-15 12:02:39 +0100

    .

--- app/src/main/java/com/example/pendulumwatch/MainActivity.kt
+++ app/src/main/java/com/example/pendulumwatch/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.pendulumwatch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pendulumwatch.ui.theme.PendulumWatchTheme
-import javax.swing.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +39,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
-
+    Canvas(
+        Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .aspectRatio(1f)
+    ){
+        drawRect(Color.White, style = Stroke(width = 2.dp.toPx()))
+    }
 }
 
