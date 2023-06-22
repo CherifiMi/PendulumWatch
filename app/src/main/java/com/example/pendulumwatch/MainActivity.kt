4da0abaff1fe8cb5c17cca54f5a00d9041702623
CherifiMi <cherifi154949@gmail.com>
2023-06-22 03:07:15 +0100

    all

--- app/src/main/java/com/example/pendulumwatch/MainActivity.kt
+++ app/src/main/java/com/example/pendulumwatch/MainActivity.kt
@@ -15,12 +15,14 @@ import androidx.compose.foundation.progressSemantics
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
@@ -58,6 +60,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Pend(modifier: Modifier = Modifier) {
+
+    var spining by remember {
+        mutableStateOf(90f)
+    }
+    var scaling by remember {
+        mutableStateOf(1f)
+    }
+
+
     var isStuck by remember {
         mutableStateOf(false)
     }
@@ -68,7 +79,7 @@ fun Pend(modifier: Modifier = Modifier) {
         mutableStateOf(Offset(0f, 0f))
     }
     var angle by remember {
-        mutableStateOf(100f)
+        mutableStateOf(0f)
     }
     var aVelocity by remember {
         mutableStateOf(0f)
@@ -87,6 +98,9 @@ fun Pend(modifier: Modifier = Modifier) {
     }
     var r by remember {
         mutableStateOf(800f)
+    }
+    var isMoving by remember {
+        mutableStateOf(false)
     }
 
     val update by rememberInfiniteTransition().animateFloat(
@@ -114,10 +128,29 @@ fun Pend(modifier: Modifier = Modifier) {
             var c2 = origen.minus(Offset(pressPos.x, pressPos.y))
             angle = (atan2(-1 * c2.y, c2.x) - (90f * PI / 180f)).toFloat()
         }
+
+        spining -= 0.5f
+        if (spining < 0) {
+            spining = 90f
+        }
+
+        if (angle > 0.7f) {
+            isStuck = false
+        }
+
+        isMoving = (isStuck && angle != 0f) || (!isStuck && angle == 0f)
+
+        if (!isMoving) {
+            scaling = 0.83f
+        } else {
+            scaling = 1f
+        }
     }
+
 
     Canvas(
         modifier
+            .scale(scaling)
             .pointerInput(Unit) {
                 detectDragGestures(
                     onDragStart = {
@@ -131,30 +164,48 @@ fun Pend(modifier: Modifier = Modifier) {
             }
     ) {
         origen = Offset(40f, 0f)
+        translate(if (isMoving) 0f else 220f, if (isMoving) 0f else -150f) {
 
-        drawRect(Color.White, style = Stroke(2.dp.toPx()))
 
-        drawArc(
-            color = Blu,
-            startAngle = 50f,
-            sweepAngle = 40f,
-            useCenter = false,
-            size = Size(r * 2, r * 2),
-            topLeft = Offset(origen.x - r, -r),
-            style = Stroke(
-                width = 4.dp.toPx(),
-                pathEffect = PathEffect.dashPathEffect(floatArrayOf(20f, 20f), 0f),
-                cap = StrokeCap.Round
+            if (isMoving) {
+                drawArc(
+                    color = Blu,
+                    startAngle = 50f,
+                    sweepAngle = 40f,
+                    useCenter = false,
+                    size = Size(r * 2, r * 2),
+                    topLeft = Offset(origen.x - r, -r),
+                    style = Stroke(
+                        width = 4.dp.toPx(),
+                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(15f, 15f), 0f),
+                        cap = StrokeCap.Round
+                    )
+                )
+
+                drawArc(
+                    color = Color.White,
+                    alpha = if (spining > 90 || spining < 50) 0f else 0.7f,
+                    startAngle = spining,
+                    sweepAngle = 3f,
+                    useCenter = false,
+                    size = Size(r * 2, r * 2),
+                    topLeft = Offset(origen.x - r, -r),
+                    style = Stroke(
+                        width = 4.dp.toPx(),
+                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(15f, 15f), 0f),
+                        cap = StrokeCap.Round
+                    )
+                )
+            }
+
+            drawLine(
+                Grn,
+                start = origen,
+                end = location,
+                strokeWidth = 2.dp.toPx()
             )
-        )
-
-        drawLine(
-            Grn,
-            start = origen,
-            end = location,
-            strokeWidth = 2.dp.toPx()
-        )
-        drawCircle(if (isStuck) Red else Grn, radius = 5*8f, center = location)
+            drawCircle(if (isStuck) Red else Grn, radius = 5 * 8f, center = location)
+        }
     }
 }
 
